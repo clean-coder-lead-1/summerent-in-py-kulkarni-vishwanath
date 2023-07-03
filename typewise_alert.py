b5d71aade8e09a6e2b24e5e72d9d9179a358395a
Vishwanath R Kulkarni <kulkarni.vishwanath1@gmail.com>
2023-07-04 01:47:04 +0530

    Update typewise_alert.py
    Modifications made as per requirement.

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,46 +1,83 @@
 
-def infer_breach(value, lowerLimit, upperLimit):
-  if value < lowerLimit:
-    return 'TOO_LOW'
-  if value > upperLimit:
-    return 'TOO_HIGH'
-  return 'NORMAL'
+def infer_breach(value, temperature_range):
+    """Checks the breach based on the temperature value and the range.
+
+    :param value: temperature threshold
+    :type value: int
+    :param temperature_range: temperature range
+    :type temperature_range: list
+    :return: breach
+    :rtype: string
+    """
+    if value < temperature_range[0]:
+        return 'TOO_LOW'
+    if value > temperature_range[1]:
+        return 'TOO_HIGH'
+    return 'NORMAL'
 
 
-def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
-  return infer_breach(temperatureInC, lowerLimit, upperLimit)
+def classify_temperature_breach(cooling_type, temperature_inc):
+    """classify the temperature breach based on cooling type and temperature threshold.
+
+    :param cooling_type: type of cooling
+    :type cooling_type: string
+    :param temperature_inc: temperature threshold
+    :type temperature_inc: int
+    :return: breach
+    :rtype: string
+    """
+    temperature_range = list()
+    if cooling_type == 'PASSIVE_COOLING':
+        temperature_range.extend([0,35])
+    elif cooling_type == 'MED_ACTIVE_COOLING':
+        temperature_range.extend([0,40])
+    elif cooling_type == 'HI_ACTIVE_COOLING':
+        temperature_range.extend([0,45])  
+    return infer_breach(temperature_inc, temperature_range)
 
 
-def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType =\
-    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
-  elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
+def check_and_alert(alert_target, battery_char, temperature_inc):
+    """Send mail to controller or email id based on alert mechanism.
+
+    :param alert_target: whom to alert.
+    :type alert_target: string
+    :param battery_char: cooling type
+    :type battery_char: int
+    :param temperature_inc: temperature threshold
+    :type temperature_inc: int
+    """
+    breach_type = classify_temperature_breach(battery_char['coolingType'], temperature_inc)
+    if alert_target == 'TO_CONTROLLER':
+        send_to_controller(breach_type)
+    elif alert_target == 'TO_EMAIL':
+        send_to_email(breach_type)
 
 
-def send_to_controller(breachType):
-  header = 0xfeed
-  print(f'{header}, {breachType}')
+def send_to_controller(breach_type):
+    """Send information to controller based on the breach type.
+
+    :param breach_type: breach type
+    :type breach_type: string
+    :return: content to controller.
+    :rtype: string
+    """
+    header = 0xfeed
+    print(f'{header}, {breach_type}')
+    return f'{header}, {breach_type}'
 
 
-def send_to_email(breachType):
-  recepient = "a.b@c.com"
-  if breachType == 'TOO_LOW':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too low')
-  elif breachType == 'TOO_HIGH':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too high')
+def send_to_email(breach_type):
+    """Send mail to recepient based on breach type.
+
+    :param breach_type: breach type
+    :type breach_type: string
+    :return: mail content to recepient
+    :rtype: string
+    """
+    recepient = "a.b@c.com"
+    if breach_type == 'TOO_LOW':
+        message = f'Hi {recepient}, the temperature is {breach_type}'
+    elif breach_type == 'TOO_HIGH':
+        message = f'Hi {recepient}, the temperature is {breach_type}'
+    print (message)
+    return message
